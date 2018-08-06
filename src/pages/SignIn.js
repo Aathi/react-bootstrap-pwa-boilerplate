23fe7aa32ad5a31548aadc66356df4d17c0a0e58
Ponnampalam Athithan <aathi@7zero.com>
2018-08-06 23:38:42 +0200

    sign in redirect conf

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,20 +1,16 @@
 import React, { Component } from "react";
-import '../styles/signIn.css'
-import {Scholarship} from '../helpers/Icons'
+import { withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "../styles/signIn.css";
+import { Scholarship } from "../helpers/Icons";
+import Button from "../components/common/Button";
 
-
-export default class Signin extends Component {
+class Signin extends Component {
   render() {
     return (
       <div>
         <form className="form-signin">
-          <img
-            className="mb-4"
-            src={Scholarship}
-            alt
-            width={72}
-            height={72}
-          />
+          <img className="mb-4" src={Scholarship} alt width={72} height={72} />
           <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
           <label htmlFor="inputEmail" className="sr-only">
             Email address
@@ -42,12 +38,16 @@ export default class Signin extends Component {
               <input type="checkbox" defaultValue="remember-me" /> Remember me
             </label>
           </div>
-          <button className="btn btn-lg btn-primary btn-block" type="submit">
-            Sign in
-          </button>
+          <Button
+            className="btn btn-lg btn-primary btn-block"
+            label="Sign in"
+            url="/"
+          />
           <p className="mt-5 mb-3 text-muted">© 2017-2018</p>
         </form>
       </div>
     );
   }
 }
+
+export default withRouter(Signin);
